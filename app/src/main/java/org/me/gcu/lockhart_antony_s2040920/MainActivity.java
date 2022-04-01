316570064a646710c254de334ac2c1bbc3e5a28f
castortroy05 <44813655+castortroy05@users.noreply.github.com>
2022-04-01 20:47:23 +0100

    AndroidX Migration

--- app/src/main/java/org/me/gcu/lockhart_antony_s2040920/MainActivity.java
+++ app/src/main/java/org/me/gcu/lockhart_antony_s2040920/MainActivity.java
@@ -2,7 +2,7 @@ package org.me.gcu.lockhart_antony_s2040920;
 
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
--- app/src/main/java/org/me/gcu/lockhart_antony_s2040920/ResultsActivity.java
+++ app/src/main/java/org/me/gcu/lockhart_antony_s2040920/ResultsActivity.java
@@ -1,12 +1,7 @@
 package org.me.gcu.lockhart_antony_s2040920;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.RecyclerView;
-import android.widget.ListView;
-
-
-import java.util.List;
 
 public class ResultsActivity extends AppCompatActivity {
 
