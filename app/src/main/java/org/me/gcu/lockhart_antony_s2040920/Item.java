ccb426997f7a6a11eae4d1aac7f5cda7031c00f8
Antony Lockhart <antony.lockhart@gmail.com>
2022-04-04 11:46:35 +0100

    modulisation in parts

---
+++ app/src/main/java/org/me/gcu/lockhart_antony_s2040920/Item.java
@@ -0,0 +1,93 @@
+package org.me.gcu.lockhart_antony_s2040920;
+
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Item {
+    public Item(String title, String link, String description, String location, String date, String author, String comments, String pubDate, Date endDate) {
+        this.title = title;
+        this.link = link;
+        this.description = description;
+        this.location = location;
+        this.date = date;
+        this.author = author;
+        this.comments = comments;
+        this.pubDate = pubDate;
+        this.endDate = endDate;
+    }
+
+    public  String title;
+    public  String link;
+    public String description;
+    public  String location;
+    public  String date;
+    private String author;
+    private String comments;
+    private String pubDate;
+
+
+    private Date endDate;
+    private final int daysToComplete = 0;
+//        public final int uuid;
+
+    public Item(String title, String description, String link, String location, String date) {
+        this.title = title;
+        this.description = description;
+        this.link = link;
+        this.location = location;
+        this.date = date;
+//            this.uuid = Integer.parseInt(UUID.randomUUID().toString());
+    }
+
+    public void setDescription(String description)  {
+
+        if(description.contains("<br />")){
+
+        String[] info = description.split("<br />");
+
+        //Log.e("FULLINFO", description.toString());
+
+        String endDateStr = "";
+        String startDateStr;
+
+        startDateStr = info[0];
+
+        if(info.length > 1)
+            endDateStr = info[1];
+
+        if(info.length>2){
+
+            String[] di = info[2].split(": ");
+
+            String delayInformation = "";
+            if(di.length>1)
+                delayInformation = di[1];
+        }
+
+        startDateStr = startDateStr.substring(12);
+        startDateStr.trim();
+        //startDateStr = startDateStr.
+        endDateStr = endDateStr.substring(10);
+        Date startDate = null;
+        Date endDate = null;
+
+        try {
+            startDate = new SimpleDateFormat("EE, dd MMMM yyyy - kk:mm").parse(startDateStr);
+            endDate = new SimpleDateFormat("EE, dd MMMM yyyy - kk:mm").parse(endDateStr);
+        } catch (ParseException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+
+            Date startDate1 = startDate;
+        this.endDate = endDate;
+        }
+        this.description = description;
+    }
+    }
+
+
+
--- app/src/main/java/org/me/gcu/lockhart_antony_s2040920/ItemAdapter.java
+++ app/src/main/java/org/me/gcu/lockhart_antony_s2040920/ItemAdapter.java
@@ -7,24 +7,22 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
-import org.me.gcu.lockhart_antony_s2040920.PullParser.Item;
+import org.me.gcu.lockhart_antony_s2040920.Item;
 
 import java.util.List;
 
 public class ItemAdapter extends ArrayAdapter<Item> {
 
     private final List<Item> itemList;
-    private int layoutResource;
+
     public ItemAdapter(Context context, int resource, List<Item> itemList) {
         super(context, resource, itemList);
-        this.layoutResource =resource;
         this.itemList = itemList;
     }
 
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        View view = convertView;
 
         Item item = getItem(position);
         if (convertView == null) {
@@ -32,8 +30,13 @@ public class ItemAdapter extends ArrayAdapter<Item> {
         }
         TextView itemName = convertView.findViewById(R.id.title);
         TextView itemDescription = convertView.findViewById(R.id.description);
+        TextView itemLink = convertView.findViewById(R.id.link);
+        TextView itemLocation =convertView.findViewById(R.id.location);
+
         itemName.setText(item.title);
         itemDescription.setText(item.description);
+        itemLink.setText(item.link);
+        itemLocation.setText(item.location);
         return convertView;
     }
 
